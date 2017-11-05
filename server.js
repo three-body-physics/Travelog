9c4791b8cac0ae3ff4511a31a8fc6cd72c51439d
three-body-physics <mintpie@googlemail.com>
2017-11-05 12:23:42 +0000

    footer

--- server.js
+++ server.js
@@ -48,10 +48,15 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 5000;
+var router = express.Router();
 
-app = express();
+var app = express();
 
 app.use(serveStatic(__dirname + "/dist"));
+app.use(router);
 
+router.get('/*', function (req, res) {  
+     return res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+});
 
 app.listen(port);
