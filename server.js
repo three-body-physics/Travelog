7a12a732469a90ec53ad61e74958d5163af960b3
three-body-physics <mintpie@googlemail.com>
2017-11-05 09:38:54 +0000

    reconfig api route

--- server.js
+++ server.js
@@ -24,7 +24,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const router = express.Router();
 
 app.use(express.static(__dirname + "/dist"));
