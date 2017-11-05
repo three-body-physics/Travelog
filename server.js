013d2faa7f9035bc2db29adfa915aa9a2b937a04
three-body-physics <mintpie@googlemail.com>
2017-11-05 09:12:16 +0000

    05112017

---
+++ server.js
@@ -0,0 +1,42 @@
+// var express = require("express");
+// var app = express();
+//
+// var path = require("path");
+// var bodyParser = require("body-parser");
+//
+//
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }));
+//
+// app.use(bodyParser.json())
+//
+//
+// // app.set("views", path.join(__dirname, "backend", "views"))
+//
+//
+// app.use(express.static(__dirname + '/dist'));
+//
+// var routes = require("./backend/routes/router");
+// app.use("/", routes);
+//
+// var server = require('http').Server(app).listen(3000, "localhost");
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const router = express.Router();
+
+app.use(express.static(__dirname + "/dist"));
+
+app.engine(".html", require("ejs").renderFile);
+
+app.set('views', __dirname + "/dist");
+
+router.get('/*', (req, res, next) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.use('/', router);
+
+app.listen(port);
